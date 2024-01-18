997b050eb97ed27c6a242236f04c5885a3365809
ramanvanakalla <ramanvanakalla123@gmail.com>
2024-01-18 23:24:15 +0530

    bg headers

--- src/components/Bgheader.tsx
+++ src/components/Bgheader.tsx
@@ -1,19 +1,30 @@
-import { Link } from 'react-router-dom';
-const  BGHeaders = () => {
+import { Link, useLocation } from 'react-router-dom';
+
+const BGHeaders = () => {
+  const location = useLocation();
+  const isRegisterPage = location.pathname === '/register';
+
   return (
     <div className="justify-center lg:my-12">
-         <div className="flex justify-center">
-           <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-            Artha
-          </h1>
-         </div>
-          
-         <div className="flex justify-center my-1">
-          New to Artha? <Link to='/register' className="px-2 text-blue-500 hover:underline">Create Account</Link>
-         </div>
+      <div className="flex justify-center">
+        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+          Artha
+        </h1>
+      </div>
+
+      <div className="flex justify-center my-1">
+        {isRegisterPage ? (
+          <>
+            Already have an account? <Link to="/login" className="px-2 text-blue-500 hover:underline">Sign In</Link>
+          </>
+        ) : (
+          <>
+          New to Artha? <Link to="/register" className="px-2 text-blue-500 hover:underline">Create Account</Link>
+          </>
+        )}
+      </div>
     </div>
   );
 };
-
 
 export default BGHeaders;
