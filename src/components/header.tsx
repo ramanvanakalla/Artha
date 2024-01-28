89d9d3547d691cf6ec1544a10f28e03910b111ae
ramanvanakalla <ramanvanakalla123@gmail.com>
2024-01-28 18:13:36 +0530

    nv bar in row for all

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -9,7 +9,7 @@ const Headers = () => {
       <nav className='flex'>
         <Link to='/' className="px-10 py-2 text-center font-semibold font-serif text-xl tracking-widest text-black">Artha</Link>
         <div className='flex justify-between w-full'>
-          <ul className='flex flex-col lg:flex-row md:flex-row'>
+          <ul className='flex flex-row'>
             <li className={`px-4 py-2 ${location.pathname === '/' ? 'text-black' : ''}`}>
               <Link to="/transactions" className={`text-center text-sm ${location.pathname === '/transactions' ? 'text-black' : 'text-slate-500'} hover:text-black font-sans`}>Transactions & Categories</Link>
             </li>
